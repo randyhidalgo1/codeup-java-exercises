fae21f0a93967e60c1573e7b1f05f455ab777230
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-04-11 08:57:37 -0500

    ControlStatementLoopsLecture

---
+++ src/StringExercise.java
@@ -0,0 +1,17 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+        String message1 = "We don't need no education\nWe don't need no thought control";
+        System.out.println(message1);
+        String message2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(message2);
+        String message3 = "In windows, the main drive is usually C:\\";
+        System.out.println(message3);
+        String message4 = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(message4);
+
+
+
+
+    }
+}
