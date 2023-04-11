f5f395db7b0a7b174c31a393081334f148a9c444
Randy Hidalgo <randy.x.hidalgo@gmail.com>
2023-04-11 10:01:47 -0500

    ControlStatementLoopsLecture

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,65 +1,62 @@
 import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//
-//         int i = 5;
-//         while (i <= 15){
-//             System.out.println(+i);
-//             i++;
-//         }
-//
-//        int j = 0;
-//         do {
-//             System.out.println(j);
-//             j+=2;
-//         } while (j < 100);
-//
+
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(+i);
+//            i++;
+//        }
+
+        int j = 0;
+        do {
+            System.out.println(j);
+            j += 2;
+        } while (j <= 100);
+
 //// Alter your loop to count backwards by 5's from 100 to -10.
-//        int k = 100;
-//        do {
-//            System.out.println(k);
-//            k-=5;
-//        } while (k >=-10);
+        int k = 100;
+        do {
+            System.out.println(k);
+            k -= 5;
+        } while (k >= -10);
 //
 //// Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
 //
-//          long l = 2;
-//            do {
-//                System.out.println(l);
-//                l*= l;
-//            } while (l < 1000000);
-//
-//
-//   for (int m = 100; m >=-10; m-=5) {
-//           System.out.println(m);
-//       }
-//
-//   for (long n = 2; n < 1000000; n*=n) {
-//           System.out.println(n);
-//       }
-//
+        long l = 2;
+        do {
+            System.out.println(l);
+            l *= l;
+        } while (l < 1000000);
+
+
+        for (int m = 100; m >= -10; m -= 5) {
+            System.out.println(m);
+        }
+
+        for (long n = 2; n < 1000000; n *= n) {
+            System.out.println(n);
+        }
+
 //// FizzBuzz
-//        for (int o = 1; o <= 100; o++) {
-//            System.out.println(o);
-//        }
-//
-//        for (int p = 1; p <= 100; p *= 3) {
-//            System.out.println("Fizz");
-//        }
-//
-//        for (int q = 1; q <= 100; q *= 5) {
-//            System.out.println("Buzz");
-//        }
-//
-//        for (int r = 1; r <= 100; r *= 15) {
-//            System.out.println("FizzBuzz");
-//        }
+        for (int o = 1; o <= 100; o++) {
+            if (o % 3 == 0 && o % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (o % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (o % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(o);
+            }
+        }
+
 //////       Table of Powers
 //
-//            Scanner scanner = new Scanner(System.in);
-//            String choice = "y";
+//        Scanner scanner = new Scanner(System.in);
+//        String choice = "y";
 //
-//            do {
+//        do {
 //            System.out.print("Enter an number: ");
 //            int num = scanner.nextInt();
 //
@@ -67,14 +64,15 @@ public class ControlFlowExercises {
 //            System.out.printf("%-10s%-10s%s%n", "======", "=======", "=====");
 //
 //            for (int i = 1; i <= num; i++) {
-//            int squared = i * i;
-//            int cubed = i * i * i;
-//            System.out.printf("%-10d%-10d%d%n", i, squared, cubed);
+//                int squared = i * i;
+//                int cubed = i * i * i;
+//                System.out.printf("%-10d%-10d%d%n", i, squared, cubed);
 //            }
 //
 //            System.out.print("Do you want to continue? (y/n) ");
 //            choice = scanner.next();
-//            } while (choice.equalsIgnoreCase("y"));
+//        } while (choice.equalsIgnoreCase("y"));
+//    }
 //}
 
 
@@ -83,7 +81,7 @@ public class ControlFlowExercises {
         String choice = "y";
 
         do {
-            System.out.print("Enter a numerical grade from 0 to 100: ");
+            System.out.print("Enter a number grade from 0 to 100: ");
             int grade = scanner.nextInt();
             String letterGrade;
 
